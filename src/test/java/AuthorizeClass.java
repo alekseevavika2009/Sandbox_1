e3d126aaa02a00778ea415a1b16ddf10f8f44c6e
alekseevavika2009 <89657672975Aa@>
2025-11-09 12:54:34 +0300

    correct tests + 1 new test

--- src/test/java/AuthorizeClass.java
+++ src/test/java/AuthorizeClass.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+
 import static org.testng.Assert.assertEquals;
 
 public class AuthorizeClass extends BaseTest {
@@ -18,6 +19,12 @@ public class AuthorizeClass extends BaseTest {
         browser.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("secret_sauce");
         browser.findElement(By.id("login-button")).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Products']")));
+        String currentUrl = browser.getCurrentUrl();
+        assertEquals(currentUrl, "https://www.saucedemo.com/inventory.html");
+        //isDisplayed - проверяем виден ли элемент на странице
+        boolean isProductsDisplayed = browser.findElement(By.xpath("//span[text()='Products']")).isDisplayed();
+        //assertEquals - подтверждаем, что фактическое и ожидаемое значения равны
+        assertEquals(isProductsDisplayed, true, "Products title should be displayed after login");
     }
 
     @Test
@@ -27,8 +34,17 @@ public class AuthorizeClass extends BaseTest {
         browser.findElement(By.id("user-name")).sendKeys("111");
         browser.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("secret_sauce");
         browser.findElement(By.id("login-button")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Products']")));
-        String errorText = browser.findElement(By.xpath("//span[text()='Products']")).getText();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h3[@data-test='error']")));
+        String errorText = browser.findElement(By.xpath("//h3[@data-test='error']")).getText();
         assertEquals(errorText, "Epic sadface: Username and password do not match any user in this service");
+    }
+
+    @Test
+    public void checkAuthorizationWithEmptyPassword() {
+        browser.get("https://www.saucedemo.com/");
+        browser.findElement(By.id("user-name")).sendKeys("standard_user");
+        browser.findElement(By.id("login-button")).click();
+        String errorText = browser.findElement(By.xpath("//h3[@data-test='error']")).getText();
+        assertEquals(errorText, "Epic sadface: Password is required");
     }
 }
