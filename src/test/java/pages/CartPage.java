f7037d5b4a4d804037cee710f7e326dd33a4f452
alekseevavika2009 <89657672975Aa@>
2025-11-28 14:41:44 +0300

    change in CartPage

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CartPage extends BasePage {
+    private final By productNamesLocator = By.cssSelector(".inventory_item_name");
     private final By title = By.xpath("//*[@data-test='title']");
 
     public CartPage(WebDriver driver) {
@@ -15,7 +16,7 @@ public class CartPage extends BasePage {
     }
 
     public ArrayList<String> getProductNames() {
-        List<WebElement> allProducts = driver.findElements(By.cssSelector(".inventory_item_name"));
+        List<WebElement> allProducts = driver.findElements(productNamesLocator);
         ArrayList<String> names = new ArrayList<>();
         for (WebElement product : allProducts) {
             names.add(product.getText());
